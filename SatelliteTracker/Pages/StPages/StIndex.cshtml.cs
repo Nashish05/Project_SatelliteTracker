c338c7694adb183132c828a54870cd07f19d9ead
Nashish05 <macatangaynashandrie@gmail.com>
2026-04-30 16:23:00 +0200

    primo commit

---
+++ SatelliteTracker/Pages/StPages/StIndex.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SatelliteTracker.Pages.StPages
+{
+    public class StIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
